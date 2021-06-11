5d73a277dfe6a87c1c47d973375ce318dc784680
Grras007 <32215386+Grras007@users.noreply.github.com>
2021-06-11 12:07:18 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,8 +9,8 @@
 #   region = "us-west-1"
 #   bucket = "yogi-ntt"
 #   key = "terraform-test.tfstate"
-#   access_key = "AKIAX63KK5HYCBA5WN7U"
-#   secret_key = "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"
+#   access_key = ""
+#   secret_key = ""
 #
 # }
 #}
@@ -20,8 +20,8 @@
 
 provider "aws" {
   region     = var.reg
-  access_key = "AKIAX63KK5HYCBA5WN7U"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 
 
