f9f993b3ac2b9c9803e1f3f69adf75edbaa48ee2
Grras007 <32215386+Grras007@users.noreply.github.com>
2021-06-11 12:07:37 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,8 +3,8 @@ terraform {
    region = "us-west-1"
    bucket = "yogi-ntt"
    key = "terraform-test.tfstate"
-   access_key = "AKIAX63KK5HYCBA5WN7U"
-   secret_key = "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"
+   access_key = ""
+   secret_key = ""
 
  }
 }
